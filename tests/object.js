d9a48e8683d2572d33ff58c2a3217321ef2a7580
taronpa <taron.petrosyan@picsart.com>
2016-07-01 19:00:34 +0400

    Solve issue related to Dates

--- tests/object.js
+++ tests/object.js
@@ -78,5 +78,40 @@ describe("Object Assertion", function () {
             }
         });
 
+        it('Nested Array in Object with date', function (done) {
+            try {
+                var golden = [{
+                    id: "57762f0f35f674b904b70ec5",
+                    created: "2016-07-01T08:51:27.414Z",
+                    topic: "testing topic invite",
+                    members: [
+                        198326933001102,
+                        155505700000102
+                    ],
+                    messages:[]
+                }];
+                var array =  [
+                    {
+                        id: "57762f0f35f674b904b70ec5",
+                        created: new Date('2016-07-01T08:51:27.414Z'),
+                        topic: "testing topic invite",
+                        members: [
+                            198326933001102,
+                            155505700000102
+                        ],
+                        messages:[]
+                    }
+                ];
+                comppass(array, golden);
+            } catch (ex) {
+                var err = ex;
+            } finally {
+                if (err) {
+                    return done(new Error('AssertionError should not have been risen'));
+                }
+                return done();
+            }
+        });
+
     });
 });
